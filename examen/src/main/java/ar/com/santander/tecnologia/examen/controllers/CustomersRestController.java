cd81b08eda80b31a49c304edc9f244c68ed05566
nicolasgrana <nicolas.grana.13@gmail.com>
2019-09-10 00:36:51 -0300

    lala

--- examen/src/main/java/ar/com/santander/tecnologia/examen/controllers/CustomersRestController.java
+++ examen/src/main/java/ar/com/santander/tecnologia/examen/controllers/CustomersRestController.java
@@ -2,20 +2,16 @@ package ar.com.santander.tecnologia.examen.controllers;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 import ar.com.santander.tecnologia.examen.model.Customer;
 import ar.com.santander.tecnologia.examen.service.CustomerService;
 
 
-@RestController
 public class CustomersRestController {
 
-	@Autowired
 	CustomerService customerService;
 	
 	@RequestMapping(value = "/getRestCustomers", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
